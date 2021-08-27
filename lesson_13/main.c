cdad23d54e2a7e2db064e631bb3eaf55973c92b4
Lobanov Alex <LobanovAlex94@gmail.com>
2021-08-27 18:02:39 +0300

    fix main/case f oprion

--- lesson_13/main.c
+++ lesson_13/main.c
@@ -17,38 +17,37 @@ int main (int argc, char **argv){
     struct measures_t *my_m;
     my_m = malloc(sizeof(struct measures_t) * cur_size);
     get_cur_time(&current_month, &current_year);
-    
-    /*while ((ret = getopt(argc, argv, opts)) != -1){
-        if (ret == 'f') strcpy (user_file,optarg);}   */
-    
-    //temporary solution
-    scanf("%s", &user_file[0]);
-    strcpy (user_file,user_file);
-
 
 D printf("cur sz1=%d\n",cur_size);
-    strcat (user_file,".csv");
-    data_file = fopen(user_file,"r");
-    if (data_file == NULL) printf("Can't open %s file.\n", user_file); 
-    else scan_file(my_m, data_file, &cur_size);
 
-    //Default current month + year summary printing
-    print_month_data(my_m, current_month, current_year, plug, 0);
-    print_all(my_m, current_year, current_year);
-    printf ("===============================================\n");
+    
 D printf("cur sz2=%d\n",cur_size);
+
     while ((ret = getopt(argc, argv, opts)) != -1){
         switch (ret){
+            case 'f' : 
+                strcpy (user_file,optarg);
+                strcat (user_file,".csv");
+                data_file = fopen(user_file,"r");
+                if (data_file == NULL) printf("Can't open %s file.\n", user_file); 
+                else scan_file(my_m, data_file, &cur_size);
+                break;
             case 'a' :
-            printf ("===============================================\n");
-            print_all(my_m, current_year, 0);
-            printf ("===============================================\n"); break; 
+                printf ("===============================================\n");
+                print_all(my_m, current_year, 0);
+                printf ("===============================================\n"); break; 
             case 'l' : print_data(my_m); break;
             case 'm' : print_month_data(my_m, atoi(optarg), current_year, plug, 0); break;
             case 'h' : help_function(argv[0]); break;
             case '?' : printf("Error!"); break;
         }
     }
+
+    //Default current month + year summary printing
+    print_month_data(my_m, current_month, current_year, plug, 0);
+    print_all(my_m, current_year, current_year);
+    printf ("===============================================\n");
+
 D printf("cur sz3=%d\n",cur_size);
     free(my_m);
     if (data_file != NULL) fclose(data_file);
